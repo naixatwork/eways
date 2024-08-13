5acf5c013020d9dea45f041f44d8811f6abafa8a
naixatwork <naixatwork@gmail.com>
2024-08-13 14:04:20 +0330

    feat(header): finished header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,21 @@ import { Container, Nav } from 'react-bootstrap';
 import Brand from '#/shared/brand/brand.tsx';
 import NetworkConnectivity from '#/shared/network-connectivity/networkConnectivity.tsx';
 import { Outlet } from 'react-router-dom';
+import { useRef } from 'react';
+import NavbarContext from '#/core/layout/navbarContext';
 
 const Layout = () => {
+  const navbarContentRef = useRef<HTMLDivElement | null>(null);
+
   return (
     <div className="h-100 w-100 d-flex flex-column">
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
           <Navbar.Brand className="d-flex align-items-center">
             <Brand />
+            <div className="ms-2">
+              <div ref={navbarContentRef}></div>
+            </div>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
@@ -21,7 +28,9 @@ const Layout = () => {
         </Container>
       </Navbar>
       <Container className="flex-grow-1 overflow-auto">
-        <Outlet />
+        <NavbarContext.Provider value={{ centerContentRef: navbarContentRef }}>
+          <Outlet />
+        </NavbarContext.Provider>
       </Container>
     </div>
   );
--- src/business/welcome/welcome.routes.tsx
+++ src/business/welcome/welcome.routes.tsx
@@ -1,13 +1,27 @@
 import { Route, Routes } from 'react-router-dom';
 import { Alert } from 'react-bootstrap';
+import { useContext } from 'react';
+import NavbarContext from '#/core/layout/navbarContext.ts';
+import { createPortal } from 'react-dom';
+import useAuthStore from '#/core/auth/auth.store.ts';
 
 const WelcomeRoutes = () => {
+  const { centerContentRef } = useContext(NavbarContext);
+  const { user } = useAuthStore();
+
   return (
     <Routes>
       <Route
         index
         element={
           <div className="w-100 h-100 d-flex justify-content-center align-items-center">
+            {centerContentRef.current &&
+              createPortal(
+                <p className="mb-0">
+                  <span className="fw-bold">{user.username}</span>
+                </p>,
+                centerContentRef.current
+              )}
             <Alert variant="success">
               Hi Eways team :) hope you'll like my project.
             </Alert>
